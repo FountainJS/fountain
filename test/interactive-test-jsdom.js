3207ad497a6558ddf212a942a7017849cbe00ba8
Mehdy Dara <mdara@eleven-labs.com>
2016-09-23 13:15:13 +0200

    Add interactiv test with JSDOM

---
+++ test/interactive-test-jsdom.js
@@ -0,0 +1,39 @@
+'use strict';
+
+require('co-mocha');
+
+const gulp = require('./helpers/gulp-helper');
+const yeoman = require('./helpers/yeoman-helper');
+const linter = require('./helpers/linter-helper');
+const unit = require('./helpers/unit-helper');
+const jsdom = require('./helpers/jsdom-helper');
+
+describe('fountain interactive integration test with jsdom', function () {
+  this.timeout(0);
+
+  before(function *() {
+    yield yeoman.prepare();
+  });
+
+  it(`should test linter on `, function *() {
+    const options = yield yeoman.run();
+    yield linter.linterTest(options);
+  });
+
+  it('should run unit tests', function *() {
+    const result = yield gulp.test();
+    unit.unitTests(result);
+  });
+
+  it('should run "gulp serve" and e2e on number of Techs listed', function *() {
+    const url = yield gulp.serve();
+    yield jsdom.run(url);
+    gulp.killServe();
+  });
+
+  it('should run "gulp serve:dist" and e2e on number of Techs listed', function *() {
+    const url = yield gulp.serveDist();
+    yield jsdom.run(url);
+    gulp.killServe();
+  });
+});
