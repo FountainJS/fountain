74baf3415357adc83f57b52ec0c0ccf4bb21904b
Matthieu Lux <matthieu.lux@gmail.com>
2017-02-21 21:28:22 +0100

    Bump NPM deps and NPM deps of all submodules (#63)
    * bump npm deps
    
    * bump submodules
    
    * bump submodules to last fixes
    
    * last fixes
    
    * last fix
    
    * last fixes
    
    * last fix
    
    * fix tslint usage in test following new api

--- test/helpers/linter-helper.js
+++ test/helpers/linter-helper.js
@@ -6,10 +6,10 @@ const globby = require('globby');
 const fs = require('mz/fs');
 const CLIEngine = require('eslint').CLIEngine;
 const cli = new CLIEngine({});
-const Linter = require('tslint');
+const {Linter} = require('tslint');
 const workPath = path.join(__dirname, '../../test/work');
 
-exports.linterTest = function *(options) {
+exports.linterTest = function * (options) {
   exports.eslint();
   if (options.js === 'typescript') {
     yield exports.tslint(options.framework);
@@ -20,25 +20,27 @@ exports.eslint = () => {
   const sources = [`${workPath}/conf/**/*.js`, `${workPath}/gulp_tasks/**/*.js`, `${workPath}/src/**/*.js`];
   const report = cli.executeOnFiles(sources);
   const formatter = cli.getFormatter();
-  console.log(formatter(report.results));
-
+  if (report.errorCount > 0) {
+    console.log('ESLint error', formatter(report.results));
+  }
   expect(report.errorCount).to.equal(0);
 };
 
-exports.tslint = function *(framework) {
+exports.tslint = function * (framework) {
   const extension = framework === 'react' ? 'tsx' : 'ts';
   const paths = yield globby([`${workPath}/src/**/*.${extension}`]);
   const tslintConf = yield fs.readFile(`${workPath}/tslint.json`);
   const configuration = JSON.parse(tslintConf);
-  const options = {
-    formatter: 'json',
-    configuration
-  };
+  const options = {formatter: 'json'};
   let failureCount = 0;
   for (const path of paths) {
     const contents = yield fs.readFile(path, 'utf8');
-    const ll = new Linter(path, contents, options);
-    const result = ll.lint();
+    const linter = new Linter(options);
+    linter.lint(path, contents, configuration);
+    const result = linter.getResult();
+    if (result.failureCount > 0) {
+      console.log('TSLint error', result);
+    }
     failureCount += result.failureCount;
   }
   expect(failureCount).to.equal(0);
